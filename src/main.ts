0501f1090aef89d1fab330c304bbdeb422a783e3
Evin Wilson <evinwilsonvd@gmail.com>
2023-05-25 20:12:54 +0530

    added raycask and hoverover above effect

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
+let phones: THREE.Object3D[] = [];
+
+let mouse = new THREE.Vector2();
+let raycaster = new THREE.Raycaster();
+
 // Create a scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
@@ -30,16 +35,23 @@ controls.dampingFactor = 0.1;
 
 const gltfLoader = new GLTFLoader();
 
-gltfLoader.load("./assets/mobile-shop.gltf", (gltf) => {
-  const model = gltf.scene;
-  scene.add(model);
+gltfLoader.load( "./assets/mobile-shop-collection.gltf", (gltf) => {
+    const model = gltf.scene;
+    gltf.scene.traverse((child) => {
+      if (child.name === "phones") {
+        phones.push(...child.children);
+      }
+    });
+    scene.add(model);
 
-  const boundingBox = new THREE.Box3().setFromObject(model);
-  const center = boundingBox.getCenter(new THREE.Vector3());
+    const boundingBox = new THREE.Box3().setFromObject(model);
+    const center = boundingBox.getCenter(new THREE.Vector3());
 
-  controls.target.copy(center);
+    controls.target.copy(center);
   },
-  undefined,
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
   function (e) {
     console.error(e);
   }
@@ -53,11 +65,11 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(ambientLight);
 
 // update canvas when resize happen
-window.onresize = function () {
+function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-};
+}
 
 // function to add dat.gui
 function gui_adder() {
@@ -69,7 +81,7 @@ function gui_adder() {
   // Create a separate object to hold the camera position and rotation
   const cameraValues = {
     position: camera.position.clone(),
-    rotation: camera.rotation.clone()
+    rotation: camera.rotation.clone(),
   };
 
   positionFolder.add(cameraValues.position, "x", -100, 100).name("x");
@@ -81,14 +93,14 @@ function gui_adder() {
   rotationFolder.add(cameraValues.rotation, "z", -Math.PI, Math.PI).name("z");
 
   // Update camera position and rotation when dat.gui values change
-  positionFolder.__controllers.forEach(controller => {
-    controller.onChange(function() {
+  positionFolder.__controllers.forEach((controller) => {
+    controller.onChange(function () {
       camera.position.copy(cameraValues.position);
     });
   });
 
-  rotationFolder.__controllers.forEach(controller => {
-    controller.onChange(function() {
+  rotationFolder.__controllers.forEach((controller) => {
+    controller.onChange(function () {
       camera.rotation.copy(cameraValues.rotation);
     });
   });
@@ -101,12 +113,69 @@ function gui_adder() {
   });
 }
 
+function resetMaterials() {
+  // Remove the circle pickers from the scene
+  const circlePickers = scene.children.filter(
+    (child) => child instanceof THREE.Mesh
+  );
+  for (let i = 0; i < circlePickers.length; i++) {
+    scene.remove(circlePickers[i]);
+  }
+}
+
+function hoverPieces() {
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(phones);
+  for (let i = 0; i < intersects.length; i++) {
+    let intersectedObject = intersects[i].object;
+    if (intersectedObject instanceof THREE.Mesh) {
+      
+      // Compute the bounding box of the intersected object
+      const boundingBox = new THREE.Box3().setFromObject(intersectedObject);
+      const minPoint = boundingBox.min;
+
+      // Define an offset value to position the circle picker above the lowest point
+      const offset = 0.1;
+
+      // Add a circle picker to the intersected object
+      const circleGeometry = new THREE.CircleGeometry(1, 32); // Adjust the radius and segments as desired
+      const circleMaterial = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        side: THREE.DoubleSide,
+      }); // Set the desired color
+      const circlePicker = new THREE.Mesh(circleGeometry, circleMaterial);
+
+      // Position the circle picker slightly above the lowest point of the intersected object
+      circlePicker.position.set(minPoint.x, minPoint.y + offset, minPoint.z);
+
+      // Rotate the circle picker to face upwards
+      circlePicker.rotation.x = -Math.PI / 2; // Rotate 90 degrees around the x-axis
+
+      // Add the circle picker to the scene
+      scene.add(circlePicker);
+    }
+  }
+}
+
 // Render the scene
 function animate() {
+  resetMaterials();
+  hoverPieces();
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   controls.update();
 }
 
+function onMouseMove(event) {
+  // calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
 gui_adder();
 animate();
+
+window.addEventListener("resize", onWindowResize);
+window.addEventListener("mousemove", onMouseMove, false);
